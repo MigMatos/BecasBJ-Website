e3d74af5f449a94754bfba6d47ba948565099c80
MigMatos <matosmoo2002@gmail.com>
2025-06-14 15:48:48 -0600

    2.1.3

--- index.php
+++ index.php
@@ -152,7 +152,7 @@
 
         <div class="modal fade" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
+            <div class="modal-content modal-alerts">
             <div class="modal-header">
                 <h5 class="modal-title" id="alertModalLabel">Aviso Importante</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
